8bf5ecd9218bce0423a6ef4a24a31023be603669
rami882 <80155808+rami882@users.noreply.github.com>
2021-06-12 11:22:02 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,34 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.css'
+import imgSrc from './open-graph.jpg'
 
 function App() {
     return ( 
-     <div>
-<nav class="navbar navbar-light bg-light">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">
-      Login
-    </a>
+      
+
+      <div>
+      <div style={{border:"solid 1px black",maxWidth:"100vw"}}>
+
+<h1 className="title red">Rami</h1>
+
+<br />
+
+<img src={imgSrc} />
+
+<br />
+
+<img src="font-awesome.jpg" />
+
+</div>
+
+<video width="320" height="240" controls>
+
+<source src="myVideo.mp4" type="video/mp4" />
+
+</video>
   </div>
-</nav>
-         <form>
-  <div class="mb-3">
-    <label for="exampleInputEmail1" class="form-label">Email address</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
-    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
-  </div>
-  <div class="mb-3">
-    <label for="exampleInputPassword1" class="form-label">Password</label>
-    <input type="password" class="form-control" id="exampleInputPassword1" />
-  </div>
-  <div class="mb-3 form-check">
-    <input type="checkbox" class="form-check-input" id="exampleCheck1" />
-    <label class="form-check-label" for="exampleCheck1">Check me out</label>
-  </div>
-  <button type="submit" class="btn btn-primary">Submit</button>
-</form>
-     </div>
+ 
         
 
     );
